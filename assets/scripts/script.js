1bbc627603e512c8d688ef95ae8c84bda31b089e
Yuri Souza <yskawtter77@gmail.com>
2023-09-20 14:49:21 -0300

    tooltip map add

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
 /*MENU MOBILE*/
-const menuH = document.querySelector('.menu-hamburguer')
+function menuMobileFN() {
+    const menuH = document.querySelector('.menu-hamburguer')
 const nav = document.querySelector('.nav')
 const overlay = document.querySelector('.closeOverlay')
 
@@ -15,3 +16,50 @@ overlay.addEventListener('click', function(e) {
         overlay.classList.toggle('overlay')
     }
 })
+}
+menuMobileFN()
+
+/*TOOL TIP */
+function tooltipFN() {
+    const toolTips = document.querySelectorAll('[data-tooltip]')
+    toolTips.forEach(item => {
+        item.addEventListener('mouseover', OnMouseOver)
+    })
+    function OnMouseOver(e) {
+        const tooltipBox = criarTooltipBox(this)
+        
+        onMouseMove.tooltipBox = tooltipBox
+        this.addEventListener('mousemove', onMouseMove)
+        onMouseLeave.tooltipBox = tooltipBox
+        onMouseLeave.element = this
+        this.addEventListener('mouseleave', onMouseLeave)
+    }
+    
+    const onMouseMove = {
+        handleEvent(e) {
+            this.tooltipBox.style.top = `${e.pageY + 20}px`
+            this.tooltipBox.style.left = `${e.pageX + 20}px`
+        }
+    }
+    
+    
+    const onMouseLeave =  {
+        tooltipBox: '',
+        element: '',
+        handleEvent() {
+            this.tooltipBox.remove()
+            this.element.removeEventListener('mouseleave', onMouseLeave)
+            this.element.removeEventListener('mousemove', onMouseMove)
+        }
+    }
+    
+    function criarTooltipBox(el) {
+        const tooltipBox = document.createElement('div')
+        const text = el.getAttribute('aria-label')
+        tooltipBox.classList.add('tooltip')
+        tooltipBox.innerText = text
+        document.body.appendChild(tooltipBox)
+        return tooltipBox
+    }
+}
+tooltipFN()
